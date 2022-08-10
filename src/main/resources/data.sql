0a15a95a0c40935eb4b995e11a1211199d3569c3
gacerioni <gacerioni@gmail.com>
2022-08-10 13:10:44 -0300

    Preparing for Bootcamp - local h2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO student (id, name, email, gender) VALUES (10000, 'Gabs Cerioni', 'gacerioni@gmail.com', 'MALE');
+INSERT INTO student (id, name, email, gender) VALUES (10001, 'Bart the Shiba', 'bart@gmail.com', 'NONBINARY');
+INSERT INTO student (id, name, email, gender) VALUES (10002, 'Diego Pereira', 'diego_eng@gmail.com', 'MALE');
+INSERT INTO student (id, name, email, gender) VALUES (10003, 'Luis Redda', 'reddinha@gmail.com', 'MALE');
+INSERT INTO student (id, name, email, gender) VALUES (10004, 'Gui Nog', 'guinog@gmail.com', 'MALE');
+INSERT INTO student (id, name, email, gender) VALUES (10005, 'Winona Ryder', 'wrider@gmail.com', 'FEMALE');
+INSERT INTO student (id, name, email, gender) VALUES (10006, 'Cintia Temaki', 'cin@gmail.com', 'FEMALE');
